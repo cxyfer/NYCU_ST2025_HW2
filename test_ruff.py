dd60af37706ce88d7f61eb983461418150994bb4
cxyfer <gdst.tw@gmail.com>
2025-10-22 16:19:45 +0800

    🧪 test: Add Ruff linter test case

---
+++ test_ruff.py
@@ -0,0 +1,11 @@
+"""
+A file to test the static analysis tool Ruff.
+It should report an error because the file is not formatted.
+"""
+
+def main():
+    a = 1
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
